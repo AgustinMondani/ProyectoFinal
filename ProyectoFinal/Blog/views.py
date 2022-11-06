d44f19b90fe35a18e943604cac0f09ffa04c6ec0
Agustin Mondani <a.mondani@hotmail.com>
2022-11-05 22:29:12 -0300

    primer comit del dia 05/11

--- ProyectoFinal/Blog/views.py
+++ ProyectoFinal/Blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Blog.forms import ArticuloForm, AutorForm, SeccionForm
 
-# Create your views here.
+def inicio(request):
+    return render(request, "formulario_madre.html")
+
+
+def formularion_autor(request):
+    formulario = AutorForm()
+    contexto = {"formulario" : formulario}
+    return render(request, "formulario_autor.html", context = contexto)
+
+
+def formulario_articulo(request):
+    formulario = ArticuloForm()
+    contexto = {"formulario" : formulario}
+    return render(request, "formulario_articulo.html", context = contexto)
+
+
+def formulario_seccion(request):
+    formulario = SeccionForm()
+    contexto = {"formulario" : formulario}
+    return render(request, "formulario_seccion.html", context = contexto)
